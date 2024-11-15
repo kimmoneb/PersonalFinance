ca18eca830d3eb1395ea2196fe4f993481b18ac5
KimmoneBartley <kbartley@bu.edu>
2024-11-15 09:13:36 -0500

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+//PWD: @command123
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records";
+import cors from "cors";
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+
+const mongoURI: string =
+  "mongodb+srv://<db_username>:<db_password>@cluster1.mrck2.mongodb.net/";
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("CONNECTED TO MONGODB!"))
+  .catch((err) => console.error("Failed to Connect to MongoDB:", err));
+
+app.use("/financial-records", financialRecordRouter);
+
+app.listen(port, () => {
+  console.log(`Server Running on Port ${port}`);
+});
